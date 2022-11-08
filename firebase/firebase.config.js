684eb590c804da7e185355e87cb5440d8b462dcb
HazelBCIT <ywang600@my.bcit.ca>
2022-11-08 10:32:22 -0800

    #

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_apiKey,
-    authDomain: process.env.NEXT_PUBLIC_authDomain,
-    projectId: process.env.NEXT_PUBLIC_projectId,
-    storageBucket: process.env.NEXT_PUBLIC_storageBucket,
-    messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
-    appId: process.env.NEXT_PUBLIC_appId
+    apiKey: "AIzaSyC9-HjBaUT1EH4PeRvF9qQ32Mwux9eiJBs",
+    authDomain: "fir-2ccf5.firebaseapp.com",
+    projectId: "fir-2ccf5",
+    storageBucket: "fir-2ccf5.appspot.com",
+    messagingSenderId: "894361905722",
+    appId: "1:894361905722:web:b7b7aee06ce0d8f0de2303"
 }; 
 
 const app = initializeApp(firebaseConfig);
